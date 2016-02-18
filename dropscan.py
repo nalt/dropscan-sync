f9aa29277c819dc2d32dfd187b92e01cc1c6fc6e
nalt <nalt@users.noreply.github.com>
2016-02-18 22:09:35 +0100

    Automatically add mailings to batch

--- dropscan.py
+++ dropscan.py
@@ -31,7 +31,7 @@ class Dropscan:
 	password = ""
 	session = None
 	scanbox = None;
-	folders = []
+	folders = ['.']
 
 	def __init__(self, user, password, verbose=0):
 		self.user = user
@@ -92,21 +92,50 @@ class Dropscan:
 		batches = [ b for b in batches if not b['is_sent'] or not only_unsent ]
 		return batches
 
-	def addMailingtoBatch(self, mailing_slug, batch_id=None):
+	def addMailingtoBatch(self, mailing_slug, batch=None):
 		"""
 		Adds a mailing to an existing forwarding batch.
 		mailing_slug  -- "slug" of mailing to add
-		batch_id      -- id of forwarding batch. If unspecified, the first unsent batch is used.
+		batch         -- Batch struct, as returned by getBatches. If unspecified, the first unsent batch is used.
+		Returns: added, alreadyin, nobatch, error
 		"""
-		if batch_id is None:
+		# Get batch struct, if unspecified
+		if batch is None:
 			batches = self.getBatches();
 			if len(batches) == 0:
 				if self.verbose >= 1: print "No unsent batch available"
-				return False
-			batch_id = batches[0]['id']
+				return "nobatch"
+			batch = batches[0]
+		# Check if mailing already in batch
+		for m in batch['mailings']:
+			if m['slug'] == mailing_slug:
+				if self.verbose >= 1: print "Mailing", m['slug'], "already in batch"
+				return "alreadyin"
+		# Add mailing to batch
 		r = self.session.get('https://secure.dropscan.de/scanboxes/%s/mailings/%s/forward?forwarding_batch_id=%s&src=detail' %
-			(self.scanbox, mailing_slug, batch_id));
-		return r.status_code == 200
+			(self.scanbox, mailing_slug, batch['id']));
+		ok = r.status_code == 200
+		if not ok and self.verbose >= 1: print "Failed to add mailing", mailing_slug, "to batch"
+		return "added" if ok else "error"
+
+
+	def addFolderstoBatch(self, mailings, forward_folders):
+		"""
+		Add all mailing files (envelopes) found in any of folders to forwarding batch.
+		mailings         -- Mailings struct from getList()
+		forward_folders  -- List of folders with mailings to be forwarded
+		"""
+		for m in reversed(mailings):
+			# Check for local file
+			(filename, local_file) = self.localFileMailing(m, self.TYPE.envelope, forward_folders)
+			# If file is found, 
+			if local_file is not None:
+				res = self.addMailingtoBatch(m['slug'])
+				if res == 'added':
+					print "Adding mailing to batch:", filename
+				elif res == 'error':
+					print "Error adding mailing to batch:", filename
+
 
 	def isScanned(self, mailing):
 		"""
@@ -152,9 +181,38 @@ class Dropscan:
 		Additional folder(s) to check for locally existing files  
 		"""
 		if folders is None: self.folders = []
-		else: self.folders = folders
+		else: self.folders = folders + ['.']
 		if self.verbose >= 3: print "Local folders: ", self.folders
 
+	def localFileMailing(self, mailing, type, search_folders):
+		"""
+		Create filename for given mailing and check whether it exists locally
+		in any of the given folders
+		mailing         -- Mailing struct, element from list returned by getList()
+		type            -- ... from enum self.TYPE
+		search_folders  -- List of local folders to search in for file
+		Returns: (filename, local_file)
+		filename        -- Filename for this mailing, constructed from mailing id and type
+		local_path      -- local_file found for this mailing if existing, or None
+		"""
+		m = mailing
+		# Rewrite date
+		date_ = re.search('([0-9]*)\.([0-9]*)\.([0-9]*)', m['created_at'])
+		date = date_.group(3) + '-' + date_.group(2) + '-' + date_.group(1)
+		# ID & Filenames for this mailing
+		id = (date + '_' +  m['barcode']).encode('utf8');
+		filename = id + '_' + self.TYPE.reverse_mapping[type]
+		filename += '.pdf' if (type == self.TYPE.pdf) else '.jpg'
+
+		# Check if file already exists locally
+		local_file = None
+		for folder in search_folders:
+			path = folder + '/' + filename
+			if os.path.isfile(path):
+				local_file = path
+				if self.verbose >= 3: print "Local file found: ", folder + '/' + filename
+			if self.verbose >= 10: print "Checking file", path, not local_file is None
+		return (filename, local_file)
 
 	def syncMailings(self, mailings, thumbs=False):
 		"""
@@ -162,12 +220,6 @@ class Dropscan:
 		mailings     -- struct returned from getList()
 		"""
 		for m in reversed(mailings):
-			# Rewrite data
-			date_ = re.search('([0-9]*)\.([0-9]*)\.([0-9]*)', m['created_at'])
-			date = date_.group(3) + '-' + date_.group(2) + '-' + date_.group(1)
-			# ID & Filenames for this mailing
-			id = (date + '_' +  m['barcode']).encode('utf8');
-
 			for f in [self.TYPE.thumb, self.TYPE.envelope, self.TYPE.pdf]:
 				if f == self.TYPE.thumb and not thumbs:
 					# Do not download thumbs
@@ -175,16 +227,10 @@ class Dropscan:
 				if f == self.TYPE.pdf and not self.isScanned(m):
 					# Do not try to download PDF for non-scanned mailing
 					continue
-				filename = id + '_' + D.TYPE.reverse_mapping[f]
-				filename += '.pdf' if (f == self.TYPE.pdf) else '.jpg'
-				# Check if file already exists locally
-				file_exists = False
-				for folder in ['.'] + self.folders:
-					if os.path.isfile(folder + '/' + filename):
-						file_exists = True
-						if self.verbose >= 3: print "Local file found: ", folder + '/' + filename
+				# Check for local file
+				(filename, local_file) = self.localFileMailing(m, f, self.folders)
 				# Perform download, if required:
-				if not file_exists:
+				if local_file is None:
 					res = self.downloadMailing(m, f, filename)
 					if self.verbose >= 0:
 						if res: print "Mailing stored to", filename
@@ -207,7 +253,8 @@ def demo(user, password, args):
 		print "INBOX:", D.FILTER.reverse_mapping[f]
 		l = D.getList(f)
 		for (i,m) in enumerate(l):
-			print "%2d: %s %s" % (i, m['created_at'], m['barcode'])
+			(filename,local_file) = D.localFileMailing(m, D.TYPE.envelope, D.folders)
+			print "%2d: %s %s. local envelope: %s" % (i, m['created_at'], m['barcode'], str(local_file))
 
 	print "=== Download of last mailing to demo_*.pdf / .jpg ==="
 	l = D.getList(D.FILTER.scanned)
@@ -225,6 +272,7 @@ if __name__ == '__main__':
 	parser.add_argument('-s', '--sync', action='store_true', help='One-way sync: Download missing files of all mailings to current folder')
 	parser.add_argument('--batches', action='store_true', help='List forwarding batches (only unsent)')
 	parser.add_argument('-F', '--forward_mailing', help='Add the specified mailing slug to the first existing unsent forwarding batch')
+	parser.add_argument('--forward_dir', action='append', help='Add all mailings in given directory to forwarding batch. Must use -s.')
 	parser.add_argument('-u', required=0, help='Dropscan username (may be specified in credentials file)')
 	parser.add_argument('-p', required=0, help='Dropscan password (may be specified in credentials file)')
 	parser.add_argument('--thumbs', action='store_true', help='Also sync thumbs of envelopses')
@@ -258,12 +306,20 @@ if __name__ == '__main__':
 		D = Dropscan(user, password, args.v)
 		if args.proxy:
 			D.setProxy(args.proxy)
-		D.setLocalFolders(args.dir)
+		folders = []  if args.dir is None else args.dir
+		if args.forward_dir:
+			folders += args.forward_dir
+		D.setLocalFolders(folders)
 		D.login()
 		l1 = D.getList(D.FILTER.scanned)
 		l2 = D.getList(D.FILTER.received)
+		l3 = D.getList(D.FILTER.forwarded)
 		D.syncMailings(l2, args.thumbs)
 		D.syncMailings(l1, args.thumbs)
+
+		# Auto-add mailings in folder to forward batch
+		if args.forward_dir:
+			D.addFolderstoBatch(l1+l2, args.forward_dir)
 
 	# List unsent forwarding batches
 	elif args.batches:
